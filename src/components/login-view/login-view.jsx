ae33e37cc9a57844583870d9cb7af27a28419dab
Nick Balan <nickolae.balan@gmail.com>
2022-11-23 23:22:55 +0100

    Updated the Login View layout | v2.1

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -6,9 +6,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
 
-import { Link } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 
 //imports Material UI components
 import Avatar from '@mui/material/Avatar';
