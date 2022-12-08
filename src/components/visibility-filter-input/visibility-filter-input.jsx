1e934b01e76b0d0cc8c6e0ad955de2098ddedc8a
Nick Balan <nickolae.balan@gmail.com>
2022-12-08 23:58:42 +0100

    Updated the Navbar View layout | v1.4

--- src/components/visibility-filter-input/visibility-filter-input.jsx
+++ src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -5,7 +5,7 @@ import Form from 'react-bootstrap/Form';
 import { setFilter } from '../../actions/actions';
 
 function VisibilityFilterInput(props) {
-  return <Form.Control onChange={e => props.setFilter(e.target.value)} value={props.visibilityFilter} placeholder='Search movies' />;
+  return <Form.Control onChange={e => props.setFilter(e.target.value)} value={props.visibilityFilter} placeholder='Search movies ' />;
 }
 
 export default connect(
